9ad9d695b6b288ea2e0814bd766f9155db76ce55
Jms147 <steinhauserjames3@gmail.com>
2023-03-13 11:38:59 -0700

    finished product

--- README.md
+++ README.md
@@ -1,1 +1,10 @@
 # Security-Matters
+##DESCRIPTION
+It is a password generator that randomly chooses the characters that you want. Has to be between 8-128 characters long and it may include lower and upper case letters, numbers, or special characters
+##INSTALLATION
+jsteinhauser-24.github.io/Security-Matters
+##CREDITS
+I used this youtube video as reference whenever I got stuck
+https://www.youtube.com/watch?v=v2jfGo7ztm8&t=848s
+##LICENSE
+Refer to the MIT license above
--- script.js
+++ script.js
@@ -1,4 +1,4 @@
-var generateBtn = document.querySelector("#generate");
+
 var finalpassword = [];
 var length = 8;
 var lowercaseletters = ["a","b","c","d","e","f","g","h","i","j","k","l","m","n","o","p","q","r","s","t","u","v","w","x","y","z"];
@@ -6,28 +6,34 @@ var uppercaseletters = ["A","B","C","D","E","F","G","H","I","J","K","L","M","N",
 var numbers = ["0","1","2","3","4","5","6","7","8","9"];
 var characters = [" ",";","!","@","#","$","%","&","'","(",")","*","+","=",",","-",".","/",":","<",">","?","[","]","{","}","^","_",",","|","`","~"];
 
+var generateBtn = document.querySelector("#generate");
+
 generateBtn.addEventListener("click", writePassword);
 
 function writePassword() {
     var fpass = pass(); 
     var passwordText = document.querySelector("#password");
-
+    
     if (fpass) {
         var fpassword = generatePassword();
         passwordText.value = fpassword;
     } else {
         passwordText.value = "";
     }
+    return;
 }
 function generatePassword() {
     var password = "";
+
     for (var i = 0; i < length; i++) {
         var random = Math.floor(Math.random() * finalpassword.length)
         password = password + finalpassword[random];
     }
     return password;
 }
+
 function pass() {
+    finalpassword = [];
   length = parseInt(prompt("How long do you want your password to be? 8 to 128 characters"))
 
   if(isNaN(length) || length < 8 || length > 128) {
@@ -37,30 +43,24 @@ function pass() {
 
   if (confirm("Does your Password need lowercase letters")) {
     finalpassword = finalpassword.concat(lowercaseletters);
-  } else {
-    lowercaseletters = "";
   }
+  
 
   if(confirm("Does your Password need Uppercase letters?")) {
     finalpassword = finalpassword.concat(uppercaseletters);
-  } else {
-    uppercaseletters = "";
-  }
+  } 
   if(confirm("Does your Password need numbers?")) {
     finalpassword = finalpassword.concat(numbers);
-  } else {
-    numbers = "";
-  }
+  } 
   if(confirm("Does your Password need special characters?")) {
     finalpassword = finalpassword.concat(characters);  
-  } else {
-    characters = "";
-  }
+  } 
   if(finalpassword == false) {
     alert("Please choose at least one style of character!")
-    return false
+    return;
   }
   return true;
 }
+
 
 
